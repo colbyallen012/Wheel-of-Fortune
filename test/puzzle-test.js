e460dbcb65400ade841d4af29b5353739f78b075
Colby Allen <colbyallen012@gmail.com>
2019-03-27 23:09:28 -0600

    Lint puzzle-test
    Co-authored-by: Duy Vu <duyv.thanh@gmail.com>

--- test/puzzle-test.js
+++ test/puzzle-test.js
@@ -5,12 +5,12 @@ chai.use(spies);
 
 const expect = chai.expect;
 
-describe('Puzzle', function(){
-    let puzzle;
-    beforeEach(function (){
-        puzzle = new Puzzle();
-    });
-    it('should be an instance of puzzle', function (){
-        expect(puzzle).to.be.an.instanceOf(Puzzle);
-    });
+describe('Puzzle', function() {
+  let puzzle;
+  beforeEach(function () {
+    puzzle = new Puzzle();
+  });
+  it('should be an instance of puzzle', function () {
+    expect(puzzle).to.be.an.instanceOf(Puzzle);
+  });
 })
